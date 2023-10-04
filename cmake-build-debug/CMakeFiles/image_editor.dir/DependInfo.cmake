505ea4b855ed9614f696eff899868f6f487f23a2
Mohamed Tamer <m.tamer16902@gmail.com>
2023-10-04 07:57:38 +0300

    xd

---
+++ cmake-build-debug/CMakeFiles/image_editor.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/projects/Image-Editor/classes/Filter.cpp" "CMakeFiles/image_editor.dir/classes/Filter.cpp.obj" "gcc" "CMakeFiles/image_editor.dir/classes/Filter.cpp.obj.d"
+  "C:/projects/Image-Editor/classes/Helper.cpp" "CMakeFiles/image_editor.dir/classes/Helper.cpp.obj" "gcc" "CMakeFiles/image_editor.dir/classes/Helper.cpp.obj.d"
+  "C:/projects/Image-Editor/classes/View.cpp" "CMakeFiles/image_editor.dir/classes/View.cpp.obj" "gcc" "CMakeFiles/image_editor.dir/classes/View.cpp.obj.d"
+  "C:/projects/Image-Editor/dependency/Reader.cpp" "CMakeFiles/image_editor.dir/dependency/Reader.cpp.obj" "gcc" "CMakeFiles/image_editor.dir/dependency/Reader.cpp.obj.d"
+  "C:/projects/Image-Editor/dependency/bmplib.cpp" "CMakeFiles/image_editor.dir/dependency/bmplib.cpp.obj" "gcc" "CMakeFiles/image_editor.dir/dependency/bmplib.cpp.obj.d"
+  "C:/projects/Image-Editor/main.cpp" "CMakeFiles/image_editor.dir/main.cpp.obj" "gcc" "CMakeFiles/image_editor.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/image_editor.dir/classes/Filter.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/image_editor.dir/classes/Filter.cpp.obj: \
+ C:\projects\Image-Editor\classes\Filter.cpp \
+ C:\projects\Image-Editor\classes\Filter.h
